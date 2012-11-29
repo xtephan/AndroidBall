5b47f09d09a1aa784a94c126b844ea6c08cf3a1a
Stefan Fodor <stefan@otinsoft.com>
2012-11-29 21:19:32 +0100

    paddle is moving using mouse

---
+++ Assets/Scripts/paddle.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+
+public class paddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	
+
+    RaycastHit myhit = new RaycastHit();
+    Ray myray = new Ray();
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(Input.GetMouseButton(0)) {
+			
+			 myray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			 
+		     if (Physics.Raycast(myray, out myhit)) {	
+					//transform.position.x = myhit.point.x;
+				transform.position = new Vector3(myhit.point.x,transform.position.y,transform.position.z);
+			 }
+			
+		}
+		
+	}
+}
