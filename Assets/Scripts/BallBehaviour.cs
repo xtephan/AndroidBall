472f13c413ce60e6ca0d5c5430e9fe8406bdf972
Stefan Fodor <stefan@otinsoft.com>
2012-11-30 14:02:47 +0100

    Ball velocity increases

--- Assets/Scripts/BallBehaviour.cs
+++ Assets/Scripts/BallBehaviour.cs
@@ -5,8 +5,13 @@ public class BallBehaviour : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		// Apply initial force 
 		rigidbody.AddForce(4,4,0,ForceMode.Impulse);
+		
+		//Increase the ball's velocity 
+		InvokeRepeating("IncreaseBallVelocity",2,2);
 	}
+	
 	
 	// Update is called once per frame
 	void Update () {
@@ -17,5 +22,14 @@ public class BallBehaviour : MonoBehaviour {
 			Application.LoadLevel("menuScene");
 		}
 		
+	}
+	
+	
+	//Increase ball velocity method
+	void IncreaseBallVelocity() {
+		
+		rigidbody.velocity *= 1.05f;
+		
+		Debug.Log("velocity: " + rigidbody.velocity);
 	}
 }
